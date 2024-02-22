55d1a50de95ad86e376c3f9344cf1d74f72a9892
yuehangit <87618177+yuehangit@users.noreply.github.com>
2024-02-22 16:30:38 -0500

    Add files via upload

--- DatabaseInitialization/SeparateReviews.sql
+++ DatabaseInitialization/SeparateReviews.sql
@@ -8,16 +8,10 @@ FROM
   reviews;
   
 DELETE FROM sep_reviews
-WHERE r_id IS NULL OR product_id IS NULL;
+WHERE r_id IS NULL OR product_id IS NULL OR review is NULL;
 
 ALTER TABLE sep_reviews
-ADD PRIMARY KEY (r_id,product_id);
-
-SELECT
-  kcu.column_name
-FROM
-  information_schema.key_column_usage kcu
-  JOIN information_schema.table_constraints tc ON kcu.constraint_name = tc.constraint_name
-WHERE
-  tc.table_name = 'sep_reviews'
-  AND tc.constraint_type = 'PRIMARY KEY';
+ADD PRIMARY KEY (r_id,product_id),
+ALTER COLUMN review TYPE varchar(150),
+ALTER COLUMN u_id TYPE varchar(40),
+ALTER COLUMN r_id TYPE varchar(30);
